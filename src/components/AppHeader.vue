149b2fedc093130eccb3f2988390338b24c3ad61
Mathieu Duvillers <duvillers@gmail.com>
2023-08-21 15:23:56 +0200

    images

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,7 @@
 
 <template>
   <header class="bg-gray-900 flex justify-between items-center px-6 py-4">
-    <p id="logo" class="text-3xl m-0 text-gray-50">Vue<span class="text-red-600">Flix</span></p>
+    <p id="logo" class="text-3xl m-0 text-gray-50 font-black">Vue<span class="text-red-600">Flisk</span></p>
     <nav>
       <ul class="flex items-center list-none grid-flow-row text-gray-50">
         <li class="flex items-center my-0 mx-4">
--- src/views/Apropos.vue
+++ src/views/Apropos.vue
@@ -1,10 +1,29 @@
 <script setup>
 import {ref} from 'vue';
+import Sasukette from '../assets/images/Sasukette.png';
+import Dédé from '../assets/images/Dédé.webp'
+import Mathieu from '../assets/images/Mathieu.jpg'
 const devTeam = ref [
   {
-    name : 'Mathieu',
-    role : 'Front',
-    avatar : "https://xsgames.co/randomusers/avatar.php?g=male"
+    firstName : 'Mathieu',
+    LastName : 'Duvillers',
+    role : 'Stagiaire',
+    bio : 'Expedita maxime pariatur eum placeat necessitatibus amet, laudantium nihil quaerat vitae nesciunt dolorem iusto quas ipsam, libero natus id, repellat voluptas. Culpa repellat iure a.',
+    avatar : Mathieu
+  },
+  {
+    firstName : 'Dédé',
+    lastName : 'Glingo',
+    role : 'Tech-lead',
+    bio : 'Deserunt perspiciatis voluptatum dolorum perferendis voluptatibus sed mollitia nobis, reiciendis officia nam fugiat, saepe distinctio at fuga. Sed voluptate odit provident dolorem maxime cumque ab.',
+    avatar : Dédé
+  },
+  {
+    firstName : 'Sasukette',
+    lastName : 'Dark',
+    role : 'Front/Back',
+    bio : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi corporis minima laudantium voluptate, dolores libero harum repellat ducimus voluptates, consequuntur, neque magni eveniet eligendi.',
+    avatar : Sasukette
   }
 ]
 </script>
